b342e31739b7e644ace1d430b7f52716be1b4a38
Gerard Moreno-Torres Bertran <germtf@gmail.com>
2016-05-04 01:23:33 +0100

    Add hover logic

--- src/menu.js
+++ src/menu.js
@@ -3,7 +3,22 @@ import PureRenderMixin from 'react-addons-pure-render-mixin';
 
 export const Menu = React.createClass({
   mixins: [PureRenderMixin],
+  getInitialState: function() {
+    return {hover: false};
+  },
+  onMouseOver: function () {
+    this.setState({hover: true});
+    console.log(this.state.hover);
+  },
+  onMouseOut: function() {
+    this.setState({hover: false});
+    console.log(this.state.hover);
+  },
   render: function() {
-    return (<img src={require('../img/Menu.png')} alt='bahoo'/>);
+    return (
+      <button onMouseOver={this.onMouseOver} onMouseOut={this.onMouseOut}>
+        <img src={require('../img/Menu.png')} alt='bahoo'/>
+      </button>
+    );
   }
 });
