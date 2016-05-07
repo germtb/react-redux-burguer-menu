6dacee5f501554296a4b0e948b211fdfc3a20de2
Gerard Moreno-Torres Bertran <germtf@gmail.com>
2016-05-07 16:18:52 +0100

    Add hover to button

--- src/menu.js
+++ src/menu.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 
+const buttonStyle = {
+  backgroundColor: "Transparent",
+  border: "none"
+};
+
 export const Menu = React.createClass({
   mixins: [PureRenderMixin],
+  onClick: function(argument) {
+    console.log(argument);
+  },
   getInitialState: function() {
     return {hover: false};
   },
   onMouseOver: function () {
     this.setState({hover: true});
-    console.log(this.state.hover);
   },
   onMouseOut: function() {
     this.setState({hover: false});
-    console.log(this.state.hover);
   },
   render: function() {
     return (
-      <button onMouseOver={this.onMouseOver} onMouseOut={this.onMouseOut}>
-        <img src={require('../img/Menu.png')} alt='bahoo'/>
+      <button style={buttonStyle} onClick={this.onClick} onMouseOver={this.onMouseOver} onMouseOut={this.onMouseOut}>
+        <img src={this.state.hover ? require('../img/Menu_hover.png') : require('../img/Menu.png')} alt='bahoo'/>
       </button>
     );
   }
