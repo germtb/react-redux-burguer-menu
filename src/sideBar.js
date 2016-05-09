a12047d79c7d9da56ce084b9fdfd72e7a1c425ca
Gerard Moreno-Torres Bertran <germtf@gmail.com>
2016-05-09 22:08:09 +0100

    Add first animation with react-motion

--- src/sideBar.js
+++ src/sideBar.js
@@ -1,22 +1,40 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 import {store} from './store';
+import {Motion, spring} from 'react-motion';
 
-const sideBarStyle = {
-  position: 'absolute',
-  backgroundColor: 'red',
-  width: 400,
-  left: 0,
-  top: 0,
-  bottom: 0,
+const sideBarStyle = function(left) {
+  return {
+    position: 'absolute',
+    backgroundColor: 'red',
+    width: left,
+    left: 0,
+    top: 0,
+    bottom: 0,
+  };
 };
 
 export const SideBar = React.createClass({
   mixins: [PureRenderMixin],
   render: function() {
     return (
-      <div style={sideBarStyle}>
-      </div>
+      <Motion defaultStyle={{
+        position: 'absolute',
+        backgroundColor: 'red',
+        width: 400,
+        left: -400,
+        top: 0,
+        bottom: 0,
+      }} style={{
+        position: 'absolute',
+        backgroundColor: 'red',
+        width: 400,
+        left: spring(0),
+        top: 0,
+        bottom: 0,
+      }}>
+        {interpolatingStyle => <div style={interpolatingStyle}/>}
+      </Motion>
     );
   }
 });
